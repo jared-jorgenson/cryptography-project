e357bdf602278a45448564a5229d7feecdc7e510
Jared Jorgenson <38658539+jared-jorgenson@users.noreply.github.com>
2021-05-03 00:46:48 +0100

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from ceaser_cipher import ceaser_encrypt, ceaser_decrypt
-
-right_rotataion = 10
-
-secret_encrypted = ceaser_encrypt("python is pretty cool", right_rotataion)
-secret_decrypted = ceaser_decrypt(secret_encrypted, right_rotataion)
-
-print(secret_encrypted)
-print(secret_decrypted)
